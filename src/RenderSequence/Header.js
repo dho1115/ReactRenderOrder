33cd2c8a9f1f20698fc9358681d082eb869a8fd3
DAVID HO <davidhorph@hotmail.com>
2022-07-07 02:09:06 -0500

    Added a sub-header.

--- src/RenderSequence/Header.js
+++ src/RenderSequence/Header.js
@@ -4,6 +4,9 @@ const Header = (props) => {
     return (
         <div>
             <h1 style={{color: 'crimson'}}>HEADER ADDED FROM A CLONED COPY.</h1>
+            <section>
+                <h3 style={{color: 'crimson'}}><i>This would be my sub-header</i></h3>
+            </section>
         </div>
     )
 }
